085c6ae997ccb8b37862f161401000a3cb3cff9c
Jing Xiang <jxsim.2013@gmail.com>
2021-09-20 23:36:10 +0800

    Update navbar

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,8 +5,10 @@ class Appointment < ApplicationRecord
   belongs_to :patient
 
   before_save do
-    last_id = Appointment.last.id
-    self.id = "A#{(last_id[1..last_id.length-1].to_i + 1)}"
+    if id.nil?
+      last_id = Appointment.last&.id&.sub('A', '').to_i || '0'.to_i
+      self.id = "A#{last_id + 1}"
+    end
   end
 
 end
